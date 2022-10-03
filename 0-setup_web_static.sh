36d0006dfba731885b39d9c249b94fc62267a473
captjay98WSL <captjay98@gmail.com>
2022-10-03 01:36:38 +0100

    fixxx

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,49 +1,24 @@
-#!/usr/bin/bash
-# -- Bash script that sets up your web servers for the deployment of web_static
-
-# install nginx
+#!/usr/bin/env bash
+# sets up your web servers for the deployment of web_static.
 
 sudo apt-get update
 
-sudo install nginx
+sudo apt-get -y install nginx
 
-sudo ufw allow 'nginx HTTP'
+sudo mkdir -p /data/web_static/shared/ /data/web_static/releases/test/
 
-# create the directories
-
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
-sudo touch /data/web_static/releases/test/index.html
-sudo echo '<html>
+sudo echo "<html>
   <head>
   </head>
   <body>
-    Holberton School
+	Holberton School
   </body>
-</html>' | sudo tee /data/web_static/releases/test/index.html
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
-# link directory
-
-if [ -d "/data/web_static/current" ];
-then
-    echo "/data/web_static/current already exists"
-    sudo rm -rf /data/web_static/current;
-fi;
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# change user
+sudo chown -R ubuntu:ubuntu /data
 
-sudo chown -hR $USER:$USER /data/
-
-# update nginx config
-
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-
-sudo ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-enabled/default'
-
-# restart nginx
+sudo sed -i '53i \\tlocation \/hbnb_static {\n\t\t alias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
 
 sudo service nginx restart
-
-echo -e "\e[1;32m SUCCESS!!!\e[0m"
