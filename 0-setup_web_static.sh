0fcbb7157f247b6bbf2be2aebf04a1f01ef35def
captjay98WSL <captjay98@gmail.com>
2022-10-03 01:39:56 +0100

    fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,24 +1,27 @@
 #!/usr/bin/env bash
-# sets up your web servers for the deployment of web_static.
+# sets up my web servers for the deployment of web_static
 
-sudo apt-get update
+sudo apt-get -y update
 
 sudo apt-get -y install nginx
 
-sudo mkdir -p /data/web_static/shared/ /data/web_static/releases/test/
+sudo ufw allow 'Nginx HTTP'
 
-sudo echo "<html>
-  <head>
-  </head>
-  <body>
-	Holberton School
-  </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+
+echo "<h1>ALX Africa</h1>" > /data/web_static/releases/test/index.html
+
+if [ -d "/data/web_static/current" ];
+then
+    echo "path /data/web_static/current exists"
+    sudo rm -rf /data/web_static/current;
+fi;
 
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data
 
-sudo chown -R ubuntu:ubuntu /data
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 
-sudo sed -i '53i \\tlocation \/hbnb_static {\n\t\t alias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
+sudo ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-enabled/default'
 
 sudo service nginx restart
