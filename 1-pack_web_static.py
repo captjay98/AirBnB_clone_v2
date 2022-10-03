06c758783839ab7158352fb727e90a100ef9d36f
captjay98WSL <captjay98@gmail.com>
2022-10-03 01:56:31 +0100

    done

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,10 +9,7 @@ your AirBnB Clone repo, using the function do_pack
 from fabric.api import local
 from datetime import datetime
 
-from fabric.decorators import runs_once
 
-
-@runs_once
 def do_pack():
     '''generates .tgz archive from the contents of the web_static folder'''
     local("mkdir -p versions")
